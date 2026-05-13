69dcf4c1b5736e0ba9bde409cbbd5be90b6d0a39
Keith Henderson <keith.donaldh@gmail.com>
2026-05-13 19:50:33 -0400

    update

--- hypr/programs.lua
+++
@@ -1,7 +0,0 @@
--- programs.lua
--- App / command variables (globals so keybinds.lua can access them)
-
-terminal    = "kitty"
-fileManager = "kitty mc"
-menu        = 'rofi -sort -disable-history -show drun -font "Jetbrains Mono Nerd Font 10" -icon-theme "Papirus" -show-icons -display-drun "Launch"'
-lockscreen  = "hyprlock"
